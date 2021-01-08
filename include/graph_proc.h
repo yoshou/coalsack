bb84db5569b31bf0f6bc5291ddc117d1200975f8
yoshou <yoshou.dev@gmail.com>
2021-01-09 08:27:48 +0900

    Add mux node

--- include/graph_proc.h
+++ include/graph_proc.h
@@ -1479,3 +1479,106 @@ public:
 
 CEREAL_REGISTER_TYPE(clock_buffer_node)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, clock_buffer_node)
+
+class mux_node : public graph_node
+{
+    graph_edge_ptr output;
+
+public:
+    mux_node()
+        : graph_node(), output(std::make_shared<graph_edge>(this))
+    {
+        set_output(output);
+    }
+
+    virtual std::string get_proc_name() const override
+    {
+        return "mux_node";
+    }
+
+    template <typename Archive>
+    void serialize(Archive &archive)
+    {
+    }
+
+    virtual void process(std::string input_name, graph_message_ptr message) override
+    {
+        auto msg = std::make_shared<object_message>();
+        msg->add_field(input_name, message);
+        output->send(msg);
+    }
+};
+
+CEREAL_REGISTER_TYPE(mux_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, mux_node)
+
+class demux_node : public graph_node
+{
+public:
+    demux_node()
+        : graph_node()
+    {
+    }
+
+    graph_edge_ptr add_output(std::string name)
+    {
+        auto outputs = get_outputs();
+        auto it = outputs.find(name);
+        if (it == outputs.end())
+        {
+            auto output = std::make_shared<graph_edge>(this);
+            set_output(output, name);
+            return output;
+        }
+        return it->second;
+    }
+
+    virtual std::string get_proc_name() const override
+    {
+        return "demux_node";
+    }
+
+    template <typename Archive>
+    void save(Archive &archive) const
+    {
+        std::vector<std::string> output_names;
+        auto outputs = get_outputs();
+        for (auto output: outputs)
+        {
+            output_names.push_back(output.first);
+        }
+        archive(output_names);
+    }
+
+    template <typename Archive>
+    void load(Archive &archive)
+    {
+        std::vector<std::string> output_names;
+        archive(output_names);
+        for (auto output_name : output_names)
+        {
+            set_output(std::make_shared<graph_edge>(this), output_name);
+        }
+    }
+
+    virtual void process(std::string input_name, graph_message_ptr message) override
+    {
+        if (auto obj_msg = std::dynamic_pointer_cast<object_message>(message))
+        {
+            for (auto field : obj_msg->get_fields())
+            {
+                try
+                {
+                    get_output(field.first)->send(field.second);
+                }
+                catch (const std::invalid_argument &e)
+                {
+                    spdlog::warn(e.what());
+                }
+            }
+        }
+    }
+};
+
+CEREAL_REGISTER_TYPE(demux_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, demux_node)
