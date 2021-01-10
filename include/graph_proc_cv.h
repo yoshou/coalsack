3bf97ef8da0ac870aa9ddd3728373e85de5392d1
yoshou <yoshou.dev@gmail.com>
2021-01-10 12:21:56 +0900

    Add threshold node

--- include/graph_proc_cv.h
+++ include/graph_proc_cv.h
@@ -8,6 +8,7 @@
 
 #include "graph_proc_img.h"
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 class image_viz_node: public graph_node
 {
@@ -283,3 +284,132 @@ public:
 
 CEREAL_REGISTER_TYPE(video_viz_node)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, video_viz_node)
+
+static int convert_to_cv_type(image_format format)
+{
+    switch (format)
+    {
+    case image_format::Y8_UINT:
+        return CV_8UC1;
+    case image_format::R8G8B8_UINT:
+    case image_format::B8G8R8_UINT:
+        return CV_8UC3;
+    case image_format::R8G8B8A8_UINT:
+    case image_format::B8G8R8A8_UINT:
+        return CV_8UC4;
+    default:
+        throw std::runtime_error("Invalid image format");
+    }
+}
+
+class image_transform_node : public graph_node
+{
+    graph_edge_ptr output;
+
+public:
+    image_transform_node()
+        : graph_node()
+        , output(std::make_shared<graph_edge>(this))
+    {
+        set_output(output);
+    }
+
+    virtual void transform(const image& src_image, image& dst_image) = 0;
+
+    virtual void process(std::string input_name, graph_message_ptr message) override
+    {
+        if (auto image_msg = std::dynamic_pointer_cast<frame_message<image>>(message))
+        {
+            const auto& src_image = image_msg->get_data();
+            
+            image dst_image;
+            transform(src_image, dst_image);
+
+            auto msg = std::make_shared<frame_message<image>>();
+
+            msg->set_data(std::move(dst_image));
+            msg->set_profile(image_msg->get_profile());
+            msg->set_timestamp(image_msg->get_timestamp());
+            msg->set_frame_number(image_msg->get_frame_number());
+
+            output->send(msg);
+        }
+    }
+
+    template<typename Archive>
+    void serialize(Archive& archive)
+    {}
+};
+
+CEREAL_REGISTER_TYPE(image_transform_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, image_transform_node)
+
+class threshold_node : public image_transform_node
+{
+    double thresh;
+    double maxval;
+    int thresh_type;
+
+public:
+    threshold_node()
+        : image_transform_node()
+        , thresh(0)
+        , maxval(255)
+        , thresh_type(CV_THRESH_BINARY)
+    {}
+
+    virtual std::string get_proc_name() const override
+    {
+        return "threshold_node";
+    }
+
+    double get_threshold() const
+    {
+        return thresh;
+    }
+    void set_threshold(double value)
+    {
+        thresh = value;
+    }
+    double get_max_value()
+    {
+        return maxval;
+    }
+    void set_max_value(double value)
+    {
+        maxval = value;
+    }
+    int get_threshold_type() const
+    {
+        return thresh_type;
+    }
+    void set_threshold_type(int value)
+    {
+        thresh_type = value;
+    }
+
+    virtual void transform(const image&src_image, image& dst_image) override
+    {
+        image binary_image(src_image.get_width(), src_image.get_height(), src_image.get_bpp(), src_image.get_stride(), src_image.get_metadata_size());
+        memcpy(binary_image.get_metadata(), src_image.get_metadata(), src_image.get_metadata_size());
+        binary_image.set_format(src_image.get_format());
+        
+        int cv_type = convert_to_cv_type(src_image.get_format());
+
+        cv::Mat src_mat((int)src_image.get_height(), (int)src_image.get_width(), cv_type, (void*)src_image.get_data());
+        cv::Mat dst_mat((int)binary_image.get_height(), (int)binary_image.get_width(), cv_type, (void*)binary_image.get_data());
+
+        cv::threshold(src_mat, dst_mat, thresh, maxval, thresh_type);
+
+        dst_image = std::move(binary_image);
+    }
+
+    template<typename Archive>
+    void serialize(Archive& archive)
+    {
+        archive(thresh, maxval, thresh_type);
+    }
+};
+
+CEREAL_REGISTER_TYPE(threshold_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(image_transform_node, threshold_node)
