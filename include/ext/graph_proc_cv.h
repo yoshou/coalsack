2c09fa158fdff5d175dcbfc1a346240b79aee57e
yoshou <yoshou.dev@gmail.com>
2022-06-13 03:57:05 +0900

    Change param for fast blob detector

--- include/ext/graph_proc_cv.h
+++ include/ext/graph_proc_cv.h
@@ -17,12 +17,40 @@ namespace coalsack
 {
     class fast_blob_detector_node : public graph_node
     {
+    public:
+        struct blob_detector_params
+        {
+            double min_threshold;
+            double max_threshold;
+            double step_threshold;
+            double min_area;
+            double max_area;
+            double min_circularity;
+            double max_circularity;
+            float min_dist_between_blobs;
+            std::int32_t min_repeatability;
+
+            blob_detector_params()
+            {
+                min_threshold = 50;
+                max_threshold = 220;
+                step_threshold = 10;
+                min_area = 25;
+                max_area = 5000;
+                min_circularity = 0.8f;
+                max_circularity = std::numeric_limits<float>::max();
+                min_dist_between_blobs = 10;
+                min_repeatability = 2;
+            }
+        };
+
+    private:
         graph_edge_ptr output;
-        cv::SimpleBlobDetector::Params params;
+        blob_detector_params params;
 
     public:
         fast_blob_detector_node()
-            : graph_node(), output(std::make_shared<graph_edge>(this)), params(cv::SimpleBlobDetector::Params())
+            : graph_node(), output(std::make_shared<graph_edge>(this)), params()
         {
             set_output(output);
         }
@@ -32,15 +60,15 @@ namespace coalsack
             return "fast_blob_detector_node";
         }
 
-        const cv::SimpleBlobDetector::Params &get_parameters() const
+        const blob_detector_params &get_parameters() const
         {
             return params;
         }
-        cv::SimpleBlobDetector::Params &get_parameters()
+        blob_detector_params &get_parameters()
         {
             return params;
         }
-        void set_parameters(const cv::SimpleBlobDetector::Params &params)
+        void set_parameters(const blob_detector_params &params)
         {
             this->params = params;
         }
@@ -59,13 +87,13 @@ namespace coalsack
 
                 std::vector<circle_t> keypoints;
                 blob_detector detector(src_mat.data, width, height);
-                detector.min_dist_between_blobs = params.minDistBetweenBlobs;
-                detector.min_threshold = params.minThreshold;
-                detector.max_threshold = params.maxThreshold;
-                detector.step_threshold = params.thresholdStep;
-                detector.min_area = params.minArea;
-                detector.max_area = params.maxArea;
-                detector.min_circularity = params.minCircularity;
+                detector.min_dist_between_blobs = params.min_dist_between_blobs;
+                detector.min_threshold = params.min_threshold;
+                detector.max_threshold = params.max_threshold;
+                detector.step_threshold = params.step_threshold;
+                detector.min_area = params.min_area;
+                detector.max_area = params.max_area;
+                detector.min_circularity = params.min_circularity;
                 detector.detect(keypoints);
 
                 std::vector<keypoint> pts;
@@ -92,25 +120,15 @@ namespace coalsack
         template <typename Archive>
         void serialize(Archive &archive)
         {
-            archive(params.blobColor);
-            archive(params.filterByArea);
-            archive(params.filterByCircularity);
-            archive(params.filterByColor);
-            archive(params.filterByConvexity);
-            archive(params.filterByInertia);
-            archive(params.maxArea);
-            archive(params.maxCircularity);
-            archive(params.maxConvexity);
-            archive(params.maxInertiaRatio);
-            archive(params.maxThreshold);
-            archive(params.minArea);
-            archive(params.minCircularity);
-            archive(params.minConvexity);
-            archive(params.minDistBetweenBlobs);
-            archive(params.minInertiaRatio);
-            archive(params.minRepeatability);
-            archive(params.minThreshold);
-            archive(params.thresholdStep);
+            archive(params.min_threshold);
+            archive(params.max_threshold);
+            archive(params.step_threshold);
+            archive(params.min_area);
+            archive(params.max_area);
+            archive(params.min_circularity);
+            archive(params.max_circularity);
+            archive(params.min_dist_between_blobs);
+            archive(params.min_repeatability);
         }
     };
 }
