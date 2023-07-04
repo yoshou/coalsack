64c30751fed728307f5571a55e56de471c00ec01
yoshou <yoshou.dev@gmail.com>
2023-07-04 19:00:21 +0000

    Fix test

--- test/tensor_test.cpp
+++ test/tensor_test.cpp
@@ -25,9 +25,9 @@ TEST(TensorTest, TestConstruct2)
 
     tensor<float, 4> tensor({3, 3, 1, 1}, {1, 4, 12, 1}, data.data());
 
-    ASSERT_EQ(tensor.at({0, 0, 0, 0}), 1);
-    ASSERT_EQ(tensor.at({1, 1, 0, 0}), 5);
-    ASSERT_EQ(tensor.at({2, 1, 0, 0}), 6);
+    ASSERT_EQ(tensor.get({0, 0, 0, 0}), 1);
+    ASSERT_EQ(tensor.get({1, 1, 0, 0}), 5);
+    ASSERT_EQ(tensor.get({2, 1, 0, 0}), 6);
 }
 
 TEST(TensorTest, TestConstruct3)
@@ -40,11 +40,11 @@ TEST(TensorTest, TestConstruct3)
 
     tensor<float, 4> tensor({3, 3, 1, 1}, {1, 4, 12, 1}, data.data(), {4, 1, 12, 1});
      
-    ASSERT_EQ(tensor.at({0, 0, 0, 0}), 1);
-    ASSERT_EQ(tensor.at({1, 1, 0, 0}), 5);
-    ASSERT_EQ(tensor.at({1, 2, 0, 0}), 6);
-    ASSERT_EQ(tensor.at({2, 1, 0, 0}), 8);
-    ASSERT_EQ(tensor.at({2, 2, 0, 0}), 9);
+    ASSERT_EQ(tensor.get({0, 0, 0, 0}), 1);
+    ASSERT_EQ(tensor.get({1, 1, 0, 0}), 5);
+    ASSERT_EQ(tensor.get({1, 2, 0, 0}), 6);
+    ASSERT_EQ(tensor.get({2, 1, 0, 0}), 8);
+    ASSERT_EQ(tensor.get({2, 2, 0, 0}), 9);
 }
 
 TEST(TensorTest, TestTransform1)
@@ -65,10 +65,10 @@ TEST(TensorTest, TestTransform1)
         return value + addend[h];
     });
 
-    ASSERT_EQ(result.at({0, 0, 0, 0}), 2);
-    ASSERT_EQ(result.at({1, 1, 0, 0}), 7);
-    ASSERT_EQ(result.at({2, 1, 0, 0}), 8);
-    ASSERT_EQ(result.at({2, 2, 0, 0}), 12);
+    ASSERT_EQ(result.get({0, 0, 0, 0}), 2);
+    ASSERT_EQ(result.get({1, 1, 0, 0}), 7);
+    ASSERT_EQ(result.get({2, 1, 0, 0}), 8);
+    ASSERT_EQ(result.get({2, 2, 0, 0}), 12);
 }
 
 TEST(TensorTest, TestTranspose1)
@@ -83,9 +83,9 @@ TEST(TensorTest, TestTranspose1)
 
     const auto result = tensor.transpose({1, 0, 2, 3}).contiguous();
      
-    ASSERT_EQ(result.at({0, 0, 0, 0}), 1);
-    ASSERT_EQ(result.at({1, 1, 0, 0}), 5);
-    ASSERT_EQ(result.at({1, 2, 0, 0}), 6);
-    ASSERT_EQ(result.at({2, 1, 0, 0}), 8);
-    ASSERT_EQ(result.at({2, 2, 0, 0}), 9);
+    ASSERT_EQ(result.get({0, 0, 0, 0}), 1);
+    ASSERT_EQ(result.get({1, 1, 0, 0}), 5);
+    ASSERT_EQ(result.get({1, 2, 0, 0}), 6);
+    ASSERT_EQ(result.get({2, 1, 0, 0}), 8);
+    ASSERT_EQ(result.get({2, 2, 0, 0}), 9);
 }
