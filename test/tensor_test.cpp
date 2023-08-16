b36e2feb92e72a4f1b230e1056b3adf433b8970d
yoshou <yoshou.dev@gmail.com>
2023-08-16 22:59:04 +0900

    Add test for tensor topk

--- test/tensor_test.cpp
+++ test/tensor_test.cpp
@@ -89,3 +89,60 @@ TEST(TensorTest, TestTranspose1)
     ASSERT_EQ(result.get({2, 1, 0, 0}), 8);
     ASSERT_EQ(result.get({2, 2, 0, 0}), 9);
 }
+
+TEST(TensorTest, TestTopK1)
+{
+    std::vector<float> data = {
+        4, 5, 4, 1,
+        7, 9, 1, 2,
+        1, 5, 5, 4,
+        5, 8, 6, 3
+    };
+
+    constexpr auto k = 4;
+
+    tensor<float, 1> tensor({16}, data.data());
+
+    const auto [values, indices] = tensor.view().topk(k);
+
+    std::array<float, k> sorted_values;
+    for (uint32_t i = 0; i < k; i++) {
+        sorted_values[i] = values.get({i});
+    }
+
+    std::sort(sorted_values.begin(), sorted_values.end());
+
+    ASSERT_EQ(sorted_values[0], 6);
+    ASSERT_EQ(sorted_values[1], 7);
+    ASSERT_EQ(sorted_values[2], 8);
+    ASSERT_EQ(sorted_values[3], 9);
+}
+
+TEST(TensorTest, TestTopK2)
+{
+    std::vector<float> data = {
+        4, 5, 4, 1,
+        7, 9, 1, 2,
+        1, 5, 5, 4,
+        5, 8, 6, 3
+    };
+
+    constexpr auto k = 2;
+
+    tensor<float, 2> tensor({8, 2}, data.data());
+
+    const auto [values, indices] = tensor.view().topk(k);
+
+    std::array<std::array<float, k>, 2> sorted_values;
+    for (uint32_t j = 0; j < 2; j++) {
+        for (uint32_t i = 0; i < k; i++) {
+            sorted_values[j][i] = values.get({i, j});
+        }
+        std::sort(sorted_values[j].begin(), sorted_values[j].end());
+    }
+
+    ASSERT_EQ(sorted_values[0][0], 7);
+    ASSERT_EQ(sorted_values[0][1], 9);
+    ASSERT_EQ(sorted_values[1][0], 6);
+    ASSERT_EQ(sorted_values[1][1], 8);
+}
