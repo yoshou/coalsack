7a52e488b8f6cf8cd332aa11f697397184bb0da5
yoshou <yoshou.dev@gmail.com>
2023-08-20 02:49:37 +0000

    Add depthai support

---
+++ sample/depthai-color/depthai_color.cpp
@@ -0,0 +1,132 @@
+#include <string>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <filesystem>
+#include <fstream>
+#include <boost/asio.hpp>
+
+#include <signal.h>
+#include <unistd.h>
+
+#include "graph_proc.h"
+#include "graph_proc_img.h"
+#include "graph_proc_cv.h"
+#include "ext/graph_proc_depthai.h"
+
+using namespace coalsack;
+
+static std::vector<std::function<void()>> on_shutdown_handlers;
+static std::atomic_bool exit_flag(false);
+
+static void shutdown()
+{
+    for (auto handler : on_shutdown_handlers)
+    {
+        handler();
+    }
+    exit_flag.store(true);
+}
+
+static void sigint_handler(int)
+{
+    shutdown();
+    exit(0);
+}
+
+class local_server
+{
+    asio::io_service io_service;
+    std::shared_ptr<graph_proc_server> server;
+    std::shared_ptr<std::thread> th;
+    std::atomic_bool running;
+
+public:
+    local_server()
+        : io_service(), server(std::make_shared<graph_proc_server>(io_service, "0.0.0.0", 31400)), th(), running(false)
+    {
+    }
+
+    void run()
+    {
+        running = true;
+        th.reset(new std::thread([this]
+                                 { io_service.run(); }));
+    }
+
+    void stop()
+    {
+        if (running.load())
+        {
+            running.store(false);
+            io_service.stop();
+            if (th && th->joinable())
+            {
+                th->join();
+            }
+        }
+    }
+
+    ~local_server()
+    {
+        stop();
+    }
+};
+
+int main(int argc, char *argv[])
+try
+{
+    signal(SIGINT, sigint_handler);
+
+    spdlog::set_level(spdlog::level::debug);
+
+    local_server server;
+    server.run();
+
+    asio::io_service io_service;
+
+    std::shared_ptr<subgraph> g(new subgraph());
+
+    std::shared_ptr<coalsack::depthai_color_camera_node> capture(new coalsack::depthai_color_camera_node());
+    g->add_node(capture);
+
+    std::shared_ptr<video_viz_node> viz(new video_viz_node());
+    viz->set_input(capture->get_output());
+    viz->set_image_name("video");
+    g->add_node(viz);
+
+    graph_proc_client client;
+    client.deploy(io_service, "127.0.0.1", 31400, g);
+
+    on_shutdown_handlers.push_back([&client, &server]
+                                   {
+        client.stop();
+        server.stop(); });
+
+    std::thread io_thread([&io_service]
+                          { io_service.run(); });
+
+    client.run();
+
+    while (!exit_flag.load())
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<std::int64_t>(1000.0 / 30.0)));
+        if (viz)
+        {
+            cv_window::wait_key(1);
+        }
+    }
+
+    if (io_thread.joinable())
+    {
+        io_thread.join();
+    }
+
+    return 0;
+}
+catch (std::exception &e)
+{
+    std::cout << e.what() << std::endl;
+    shutdown();
+}
