ae784884a04d702e2e2100d4a3b2c6996f3fb86b
yoshou <yoshou.dev@gmail.com>
2025-04-28 14:20:49 +0000

    fix: Fix linkage

--- include/utils.h
+++ include/utils.h
@@ -6,53 +6,53 @@
 #include <vector>
 
 namespace coalsack {
-[[maybe_unused]] static void write_string(std::ostream &ss, std::string value) {
+inline void write_string(std::ostream &ss, std::string value) {
   uint32_t length = (uint32_t)value.size();
   ss.write((const char *)&length, sizeof(length));
   ss.write((const char *)&value[0], length);
 }
 
-[[maybe_unused]] static void write_int8(std::ostream &ss, int8_t value) {
+inline void write_int8(std::ostream &ss, int8_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_uint8(std::ostream &ss, uint8_t value) {
+inline void write_uint8(std::ostream &ss, uint8_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_int16(std::ostream &ss, int16_t value) {
+inline void write_int16(std::ostream &ss, int16_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_uint16(std::ostream &ss, uint16_t value) {
+inline void write_uint16(std::ostream &ss, uint16_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_int32(std::ostream &ss, int32_t value) {
+inline void write_int32(std::ostream &ss, int32_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_uint32(std::ostream &ss, uint32_t value) {
+inline void write_uint32(std::ostream &ss, uint32_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_int64(std::ostream &ss, int64_t value) {
+inline void write_int64(std::ostream &ss, int64_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_uint64(std::ostream &ss, uint64_t value) {
+inline void write_uint64(std::ostream &ss, uint64_t value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_single(std::ostream &ss, float value) {
+inline void write_single(std::ostream &ss, float value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static void write_double(std::ostream &ss, double value) {
+inline void write_double(std::ostream &ss, double value) {
   ss.write((const char *)&value, sizeof(value));
 }
 
-[[maybe_unused]] static std::string read_string(std::istream &ss) {
+inline std::string read_string(std::istream &ss) {
   uint32_t length = 0;
   ss.read((char *)&length, sizeof(uint32_t));
 
@@ -62,37 +62,37 @@ namespace coalsack {
   return std::string(data.begin(), data.end());
 }
 
-[[maybe_unused]] static uint16_t read_uint16(std::istream &ss) {
+inline uint16_t read_uint16(std::istream &ss) {
   uint16_t value;
   ss.read((char *)&value, sizeof(value));
   return value;
 }
 
-[[maybe_unused]] static uint32_t read_uint32(std::istream &ss) {
+inline uint32_t read_uint32(std::istream &ss) {
   uint32_t value;
   ss.read((char *)&value, sizeof(value));
   return value;
 }
 
-[[maybe_unused]] static int32_t read_int32(std::istream &ss) {
+inline int32_t read_int32(std::istream &ss) {
   int32_t value;
   ss.read((char *)&value, sizeof(value));
   return value;
 }
 
-[[maybe_unused]] static int64_t read_int64(std::istream &ss) {
+inline int64_t read_int64(std::istream &ss) {
   int64_t value;
   ss.read((char *)&value, sizeof(value));
   return value;
 }
 
-[[maybe_unused]] static float read_single(std::istream &ss) {
+inline float read_single(std::istream &ss) {
   float value;
   ss.read((char *)&value, sizeof(value));
   return value;
 }
 
-[[maybe_unused]] static double read_double(std::istream &ss) {
+inline double read_double(std::istream &ss) {
   double value;
   ss.read((char *)&value, sizeof(value));
   return value;
